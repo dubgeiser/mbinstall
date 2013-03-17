17ab02873a69108db9cac5c200ca959cabcd3be2
Per <per@dubgeiser.net>
2013-03-17 01:26:23 +0100

    Add some more FF plugins and iTerm2.

--- install.sh
+++ install.sh
@@ -9,11 +9,15 @@ echo 'Install manually:'
 echo
 echo '    - Command line tools for Xcode: https://developer.apple.com/downloads/'
 echo '    - Fire Fox: http://www.mozilla.org/en-US/firefox/new/'
-echo '    - Last pass: https://lastpass.com/'
-echo '    - Xmarks: http://www.xmarks.com/'
-echo '    - FireBug: http://getfirebug.com/'
+echo '        - Last pass: https://lastpass.com/'
+echo '        - Xmarks: http://www.xmarks.com/'
+echo '        - FireBug: http://getfirebug.com/'
+echo '        - Ad blocker plus: https://addons.mozilla.org/en-US/firefox/addon/adblock-plus/'
+echo '        - Ghostery: https://addons.mozilla.org/en-US/firefox/addon/ghostery/'
+echo
 echo '    - Mysql: http://www.mysql.com/downloads/mysql/'
 echo '    - MacVim: https://github.com/b4winckler/macvim/downloads'
+echo '    - iTerm2: http://www.iterm2.com  (http://code.google.com/p/iterm2/downloads/list)'
 echo
 echo 'Make sure all versions in the file paths and package names are still'
 echo 'correct and up to date.'
